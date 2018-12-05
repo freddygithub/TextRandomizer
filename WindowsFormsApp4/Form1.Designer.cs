30edaf37849764d5b404215b0e1df86cf9fd7008
Frederick Martinez <fmartinez@tompkinsinc.com>
2018-12-04 21:16:03 -0500

    first commit

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.gridButton = new System.Windows.Forms.Button();
+            this.barcodeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // gridButton
+            // 
+            this.gridButton.Location = new System.Drawing.Point(285, 81);
+            this.gridButton.Name = "gridButton";
+            this.gridButton.Size = new System.Drawing.Size(155, 65);
+            this.gridButton.TabIndex = 0;
+            this.gridButton.Text = "Browse";
+            this.gridButton.UseVisualStyleBackColor = true;
+            this.gridButton.Click += new System.EventHandler(this.gridButton_Click);
+            // 
+            // barcodeButton
+            // 
+            this.barcodeButton.Location = new System.Drawing.Point(285, 234);
+            this.barcodeButton.Name = "barcodeButton";
+            this.barcodeButton.Size = new System.Drawing.Size(155, 65);
+            this.barcodeButton.TabIndex = 1;
+            this.barcodeButton.Text = "Browse";
+            this.barcodeButton.UseVisualStyleBackColor = true;
+            this.barcodeButton.Click += new System.EventHandler(this.barcodeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose Grid Physical File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(179, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(369, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Choose text file with barcodes";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 65);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(497, 406);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 32);
+            this.label3.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-6, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(823, 467);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.barcodeButton);
+            this.Controls.Add(this.gridButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button gridButton;
+        private System.Windows.Forms.Button barcodeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        string gridFile = null, barcodeFile = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Parent = pictureBox1;
+            label2.Parent = pictureBox1;
+            label3.Parent = pictureBox1;
+            label1.BackColor = Color.Transparent;
+            label2.BackColor = Color.Transparent;
+            label3.BackColor = Color.Transparent;
+        }
+
+        private void gridButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fbd = new OpenFileDialog();
+            fbd.Title = "Choose your input file:";
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(fbd.FileName);
+                gridFile = fbd.FileName;
+                sr.Close();
+            }
+        }
+
+        private void barcodeButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fbd = new OpenFileDialog();
+            fbd.Title = "Choose your input file:";
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(fbd.FileName);
+                barcodeFile = fbd.FileName;
+                sr.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            doWork();
+        }
+
+        public void doWork()
+        {
+            List<string> chutes = new List<string>();
+            ArrayList barcodes = new ArrayList();
+            Random rnd = new Random();
+            char level = char.MinValue;
+
+            //checks if barcodefile has been selected
+            if (barcodeFile == null)
+            {
+                MessageBox.Show("Did not select barcode file",
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                using (var reader = new StreamReader(barcodeFile))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        barcodes.Add(line);
+                    }
+                }
+            }
+
+            //checks if grid file has been selected
+            if (gridFile == null)
+            {
+                MessageBox.Show("Did not select grid physical file",
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                using (var reader = new StreamReader(gridFile))
+                {
+                    string line; int x = 0;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //this skips the first 2 lines that are usually comments
+                        if (x++ > 1)
+                        {
+                            for (int i = 0; i < line.Length; i++)
+                            {
+                                if (Char.IsWhiteSpace(line[i]))
+                                {
+                                    line = line.Substring(0, i);
+                                    break;
+                                }
+                            }
+
+                            level = line[0];
+                            line = line.Substring(1, line.Length - 1);
+
+                            chutes.Add(line);
+                        }
+                    }
+                }
+            }
+
+            //Arbitrary number
+            int howManyBarcodes = 500;
+
+            //Sort by alpha
+            chutes.Sort();
+
+            //first group always starts with A
+            char temp = chutes[0][0];
+
+            //keep track of letters
+            ArrayList placesList = new ArrayList();
+
+            //Add A to placesList
+            placesList.Add(0);
+
+            //Find where other letters start
+            for (int i = 0; i < chutes.Count; i++)
+            {
+                if (chutes[i][0] != temp)
+                {
+                    placesList.Add(i);
+                    temp = chutes[i][0];
+                }
+            }
+
+            //removing the Y chute locations because they are not same length. 
+            //placesList.RemoveAt(placesList.Count - 1);
+
+            //find out where to stop counting up
+            int endPoint = int.Parse(placesList[1].ToString());
+
+            try
+            {
+                //delete preexisting file content
+                using (var tsw = new StreamWriter(@"C:\TompkinsRobotics\barcodeRandom.txt"))
+                {
+                    tsw.Write("");
+                }
+
+                int x = 0;
+                //if we want 300 products scanned, 4 letters, 75 products per letter
+                for (int i = 0; i < (howManyBarcodes / placesList.Count); i++)
+                {
+                    //for random barcode
+                    int randomTemp = rnd.Next(0, barcodes.Count);
+
+                    if (x >= endPoint)
+                        x = 0;
+
+                    //to make sure we at least hit one barcode every time we iterate through them before doing random
+                    if (i >= barcodes.Count)
+                    {
+                        int index = i % placesList.Count;
+
+                        //if we have gone through all letters at least once, then increase index
+                        if (i > 0 && index == 0)
+                            x++;
+
+                        if (chutes[int.Parse(placesList[index].ToString())][0] == 'Y')
+                        {
+                            using (var tsw = new StreamWriter(@"C:\TompkinsRobotics\barcodeRandom.txt", true))
+                            {
+                                tsw.WriteLine(barcodes[randomTemp] + "\t" + level + chutes[int.Parse(placesList[index].ToString()) + rnd.Next(0, (chutes.Count - int.Parse(placesList[placesList.Count - 1].ToString()) - 1))] + "\t\t\t" + "default" + "\t\t\t" + "default");
+                            }
+                        }
+                        else
+                        {
+                            using (var tsw = new StreamWriter(@"C:\TompkinsRobotics\barcodeRandom.txt", true))
+                            {
+                                tsw.WriteLine(barcodes[randomTemp] + "\t" + level + chutes[int.Parse(placesList[index].ToString()) + x] + "\t\t\t" + "default" + "\t\t\t" + "default");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int index = i % placesList.Count;
+
+                        using (var tsw = new StreamWriter(@"C:\TompkinsRobotics\barcodeRandom.txt", true))
+                        {
+                            tsw.WriteLine(barcodes[i] + "\t" + level + chutes[int.Parse(placesList[index].ToString())] + "\t\t\t" + "default" + "\t\t\t" + "default");
+                        }
+                    } 
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                return;
+            }
+
+            label3.Text = "Done!";
+        }
+
+    }
+}
